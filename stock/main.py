768a7c41ccacd6f91dc4b1860b7a37e1edc90b90
unknown <yangd01234567@gmail.com>
2021-04-21 13:56:14 -0700

    added basic login verification without jwt

--- stock/main.py
+++ stock/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from .database import engine
-from .routers import stock, user
+from .routers import stock, user, authentication
 from . import models
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+app.include_router(authentication.router)
 app.include_router(stock.router)
 app.include_router(user.router)
-
---
+++ stock/routers/authentication.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from .. import schemas, database, models
+from ..hashing import Hash
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    tags=['Authentication']
+)
+
+@router.post('/login')
+def login(request: schemas.Login, db: Session =  Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Username or Password Invalid")
+
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Username or Password Invalid")
+
+    # generate jwt and return
+    return user
