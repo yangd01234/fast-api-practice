059eea42cf6c740b08a14e139ba4dad4cfff6463
unknown <yangd01234567@gmail.com>
2021-04-21 11:49:31 -0700

    Refactor: used router path operators to create prefix and tags

--- stock/routers/user.py
+++ stock/routers/user.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import Session
 from typing import List 
 from ..hashing import Hash
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/user"
+    tags=['Users']
+)
+
 get_db = database.get_db
+
 # create users
-@router.post('/user', response_model=schemas.ShowUser, tags=['users'])
+@router.post('/', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session =  Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
@@ -16,7 +21,7 @@ def create_user(request: schemas.User, db: Session =  Depends(get_db)):
     return new_user
 
 # get user without password
-@router.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+@router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id: int, db: Session =  Depends(get_db)):
     # NOTE: There is a HUGE delay if you don't put .first()
     user = db.query(models.User).filter(models.User.id == id).first()
