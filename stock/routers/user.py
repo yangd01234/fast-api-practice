0ce669b3c82eddf6a55272d543e8ea0b4e60494e
unknown <yangd01234567@gmail.com>
2021-04-21 13:36:31 -0700

    refactored: added repository folder to house operations and simplify routes

--- stock/routers/user.py
+++ stock/routers/user.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
 from .. import schemas, database, models
 from sqlalchemy.orm import Session
 from typing import List 
-from ..hashing import Hash
+from ..repository import user
 
 router = APIRouter(
-    prefix="/user"
+    prefix="/user",
     tags=['Users']
 )
 
@@ -14,20 +14,9 @@ get_db = database.get_db
 # create users
 @router.post('/', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session =  Depends(get_db)):
-    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return user.create_user(request, db)
 
 # get user without password
 @router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id: int, db: Session =  Depends(get_db)):
-    # NOTE: There is a HUGE delay if you don't put .first()
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    # use response to add a 404 no found status code
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id {id} is not avaialble")
-    return user
+    return user.get_user(id, db)
